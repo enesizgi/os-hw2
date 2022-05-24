4b562e6619fe1b0ce4ffb337ea71e5bcf5db7ca8
Enes İzgi <enesizgi@tutanota.com>
2022-05-24 03:22:33 +0300

    add cmake-build-debug folder

---
+++ cmake-build-debug/CMakeFiles/hw2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw2.dir/hw2.cpp.o"
+  "CMakeFiles/hw2.dir/hw2.cpp.o.d"
+  "CMakeFiles/hw2.dir/hw2_output.c.o"
+  "CMakeFiles/hw2.dir/hw2_output.c.o.d"
+  "hw2"
+  "hw2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/hw2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
